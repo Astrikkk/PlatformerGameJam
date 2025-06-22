c73f3269deb0d4f07254f8f6837a0af1a730f6d3
KitsuneNath <wercixx61@gmail.com>
2025-06-22 18:02:02 +0200

    Buttons

---
+++ PlatformerJam#13.09-6-22-25/Assets/Scripts/Buttons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Info()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
