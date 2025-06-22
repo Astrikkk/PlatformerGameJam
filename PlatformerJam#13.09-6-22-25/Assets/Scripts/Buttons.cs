8c47005f2dc35a7ba0d3f24599b9c9c58bdc58ea
KitsuneNath <wercixx61@gmail.com>
2025-06-22 18:32:43 +0200

    ButtonsUpdate

--- PlatformerJam#13.09-6-22-25/Assets/Scripts/Buttons.cs
+++ PlatformerJam#13.09-6-22-25/Assets/Scripts/Buttons.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
+    public GameObject Canvas;
+    public GameObject Canvas2;
+    public GameObject Canvas3;
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
+
     public void Options()
     {
-        SceneManager.LoadScene(2);
+        Canvas.gameObject.SetActive(false);
+        Canvas3.gameObject.SetActive(false);
+        Canvas2.gameObject.SetActive(true);
     }
+
     public void Info()
     {
-        SceneManager.LoadScene(3);
+        Canvas.gameObject.SetActive(false);
+        Canvas2.gameObject.SetActive(false);
+        Canvas3.gameObject.SetActive(true);
+    }
+
+    public void Menu()
+    {
+        Canvas2.gameObject.SetActive(false);
+        Canvas3.gameObject.SetActive(false);
+        Canvas.gameObject.SetActive(true);
     }
 
     public void QuitGame()
