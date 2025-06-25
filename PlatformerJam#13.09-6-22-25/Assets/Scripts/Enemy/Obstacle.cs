976ff210c03c04f9c5a8df1bb80ad25e8e386e89
duraaaaa <t8481431@gmail.com>
2025-06-25 08:26:30 +0600

    1.0

---
+++ PlatformerJam#13.09-6-22-25/Assets/Scripts/Enemy/Obstacle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private int damageCost;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerScript Hero = collision.GetComponent<PlayerScript>();
+        Hero.takeDamage(damageCost);
+    }
+}
